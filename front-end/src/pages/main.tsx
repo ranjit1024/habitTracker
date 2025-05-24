c70dcbe87b7e7008644dfd13ac70264dadd57926
ranjit <ranjitdas2048@gmail.com>
2025-05-24 20:15:23 +0530

    change

--- front-end/src/pages/main.tsx
+++ front-end/src/pages/main.tsx
@@ -1,14 +1,26 @@
 import Logo from "../assets/habit.png";
+import * as React from 'react';
+
 
 export function Main() {
+     
   return (
     <>
-    <div className="p-2">
+    <div className="">
+    <div className="flex justify-between gap-10 w-[100vw]">
 
-      <div className="flex items-center gap-1">
+      <div className="flex items-center gap-1 p-3 ">
         <img src={Logo} alt="" height={30} width={25} />
         <p className="text-[1em] text-gray-900 font-medium">H-Track</p>
       </div>
+
+      <div className="p-3 ">
+        <img className="hover:cursor-pointer active:rotate-10 duration-500 hover:scale-105" width="32" height="48" src="https://img.icons8.com/pulsar-gradient/48/lifecycle.png" alt="lifecycle"/>
+      </div>
+
+    </div>
+
+     
     </div>
     </>
   );
