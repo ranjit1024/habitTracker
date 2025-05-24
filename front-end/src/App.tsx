7d61c8a6b8a9fea9119de3494cc2037a54ed7b93
ranjit <ranjitdas2048@gmail.com>
2025-05-24 11:04:16 +0530

    main added

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Signin } from "./pages/signIn";
 import { LandingPage } from "./pages/landingPage";
-
 import { SignUp } from "./pages/singup";
+import { Main } from "./pages/main";
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/signIn" element={<Signin />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/main" element={<Main />}></Route>
       </Routes>
     </BrowserRouter>
   );
